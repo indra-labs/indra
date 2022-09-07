6a94717311c1c3c9dd7237d6128b8afe8278a31c
David Vennik <david@cybriq.systems>
2022-09-07 11:42:16 +0200

    Added start of White Paper

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "c23eb206cc0da7db1c275b641011594d8570407e"
+	ParentGitCommit = "e83cef84cad487500cfeb123a853e53416ddc853"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-07T08:33:04+02:00"
+	BuildTime = "2022-09-07T11:42:16+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.1"
+	SemVer = "v0.0.3"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indranet/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 1
+	Patch = 3
 )
 
 // Version returns a pretty printed version information string.
