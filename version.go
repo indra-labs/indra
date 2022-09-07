d54c2a23f13ad9591097e10fb89fd83964315030
David Vennik <david@cybriq.systems>
2022-09-07 13:11:33 +0200

    WP added notes about programmable routing and forwarding

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "51831c289899d46f1634145d66c819200c8c6b7f"
+	ParentGitCommit = "8f3c7ca4d485fd9ff1e71f76a1318b0da4f4c50f"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-07T12:28:59+02:00"
+	BuildTime = "2022-09-07T13:11:33+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.5"
+	SemVer = "v0.0.6"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indranet/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 5
+	Patch = 6
 )
 
 // Version returns a pretty printed version information string.
