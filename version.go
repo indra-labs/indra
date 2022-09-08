dcb3da4f16b1f8ba12a6afc41946e9c99891cc20
David Vennik <david@cybriq.systems>
2022-09-08 20:56:39 +0200

    added section about creating circuits

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "c2768fceb3754f69830fe202fa35b45d2cd32b06"
+	ParentGitCommit = "8c9e1a9eb1eba663c108ed02bf8979e269f54da6"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-08T19:36:29+02:00"
+	BuildTime = "2022-09-08T20:56:39+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.8"
+	SemVer = "v0.0.9"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indranet/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 8
+	Patch = 9
 )
 
 // Version returns a pretty printed version information string.
