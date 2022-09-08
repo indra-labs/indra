1662b8e3faebcdabc62f2686c830b0a6a8669691
David Vennik <david@cybriq.systems>
2022-09-08 19:36:29 +0200

    added stuff about session initiation

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "4fea2706c01c513f9c6eb7ccc0847c049206de23"
+	ParentGitCommit = "c2768fceb3754f69830fe202fa35b45d2cd32b06"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-08T15:11:03+02:00"
+	BuildTime = "2022-09-08T19:36:29+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.7"
+	SemVer = "v0.0.8"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indranet/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 7
+	Patch = 8
 )
 
 // Version returns a pretty printed version information string.
