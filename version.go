a8ff2c726e8fc9644733107091f78c7579da3796
David Vennik <david@cybriq.systems>
2022-09-09 10:03:32 +0200

    added note about pdf format and typora re charts

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "c20f28b8892ac089fd3c3d6f6b4e677b1b2342be"
+	ParentGitCommit = "5c735a9ec20b48881d7f9c3e09145b4598b59baf"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-09T10:01:09+02:00"
+	BuildTime = "2022-09-09T10:03:32+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.15"
+	SemVer = "v0.0.16"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indranet/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 15
+	Patch = 16
 )
 
 // Version returns a pretty printed version information string.
