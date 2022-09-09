5c735a9ec20b48881d7f9c3e09145b4598b59baf
David Vennik <david@cybriq.systems>
2022-09-09 10:01:09 +0200

    added notes about circuit parameters and payment/channels

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "e9ab4104d1edde39064872fb619c465aa38549ee"
+	ParentGitCommit = "c20f28b8892ac089fd3c3d6f6b4e677b1b2342be"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-09T09:17:23+02:00"
+	BuildTime = "2022-09-09T10:01:09+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.14"
+	SemVer = "v0.0.15"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indranet/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 14
+	Patch = 15
 )
 
 // Version returns a pretty printed version information string.
