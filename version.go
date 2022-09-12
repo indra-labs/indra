d586d8c07e90004edfe27ee34387e6bb70c7efc9
David Vennik <david@cybriq.systems>
2022-09-12 12:18:22 +0200

    small changes but big chart comparison tor and btc

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "d9d27bfeebd21456030f83a4132039cf83600d50"
+	ParentGitCommit = "622e2d0bce9662b0788831a19f72984735366a65"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-12T10:08:27+02:00"
+	BuildTime = "2022-09-12T12:18:22+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.18"
+	SemVer = "v0.0.19"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indranet/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 18
+	Patch = 19
 )
 
 // Version returns a pretty printed version information string.
