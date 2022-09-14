e01e7b99377c8f9b3620b517a51c319d969a75eb
David Vennik <david@cybriq.systems>
2022-09-14 15:54:04 +0200

    added info about cipher/accounting roots

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "fcf2a9276d9e3771f0c021b84bf49cf005ddf06e"
+	ParentGitCommit = "305497f81460857c5ea468296bdc27f38714fc71"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-12T15:55:29+02:00"
+	BuildTime = "2022-09-14T15:54:04+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.21"
+	SemVer = "v0.0.22"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indranet/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 21
+	Patch = 22
 )
 
 // Version returns a pretty printed version information string.
