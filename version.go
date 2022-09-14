b1b5042762576ff72c1c37896b3d009c7e74b6e6
David Vennik <david@cybriq.systems>
2022-09-14 19:05:02 +0200

    starting from scratch based on bolt4

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "305497f81460857c5ea468296bdc27f38714fc71"
+	ParentGitCommit = "b9422183f8d6e47eac162605a42eac531203b1ed"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-14T15:54:04+02:00"
+	BuildTime = "2022-09-14T19:05:02+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.22"
+	SemVer = "v0.0.23"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indranet/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 22
+	Patch = 23
 )
 
 // Version returns a pretty printed version information string.
