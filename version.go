72ddbe26bafac751f9dc3a2251f8e93d7d9f138e
David Vennik <david@cybriq.systems>
2022-09-15 09:02:10 +0200

    more clearly described session encryption/auth and source routing

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "05fa2760904f6bbad01d80b55702d269dc23cc1f"
+	ParentGitCommit = "44c321da589097d1a877c1cb9a5d8b8aa0093eaf"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-14T19:49:01+02:00"
+	BuildTime = "2022-09-15T09:02:10+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.24"
+	SemVer = "v0.0.26"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indranet/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 24
+	Patch = 26
 )
 
 // Version returns a pretty printed version information string.
