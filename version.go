5d6ed3c1d50773381c551632feaa8264add74248
David Vennik <david@cybriq.systems>
2022-09-17 02:54:18 +0200

    starting with onion data packet implementation

--- version.go
+++ version.go
@@ -10,19 +10,19 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "97ec7ca3382077488c26d4e1e28e186f3fc73ac2"
+	ParentGitCommit = "0a68d05fc3b7d13998006898c18f0b9e280e48c3"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-15T11:33:17+02:00"
+	BuildTime = "2022-09-17T02:54:18+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.29"
+	SemVer = "v0.0.31"
 	// PathBase is the path base returned from runtime caller.
-	PathBase = "/home/loki/src/github.com/Indra-Labs/indranet/"
+	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
 	Major = 0
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 29
+	Patch = 31
 )
 
 // Version returns a pretty printed version information string.
