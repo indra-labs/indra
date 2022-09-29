53509f8211a404fa80a940e2b51567c986b49ed4
David Vennik <david@cybriq.systems>
2022-09-29 20:49:10 +0200

    Small correction about cipher rollover and session keys

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "3eda6484448baf9de4d1e92664284bec2f2302a4"
+	ParentGitCommit = "38de439fe183b8e14af2d1f5f18faf06b40f3560"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-29T20:42:42+02:00"
+	BuildTime = "2022-09-29T20:49:10+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.36"
+	SemVer = "v0.0.37"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 36
+	Patch = 37
 )
 
 // Version returns a pretty printed version information string.
