94bd4049a08edd0107a992ac2f525bc1c291cd62
David Vennik <david@cybriq.systems>
2022-10-07 10:51:34 +0200

    Fixed error handling in schnorr package

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "b67ba7bb8d4cbadc020d09c106393ef37cfc5ee4"
+	ParentGitCommit = "af1886d0a2b3e24c0f3f7cfc60f3ab8716c22a39"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-10-06T10:22:07+02:00"
+	BuildTime = "2022-10-07T10:51:34+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.40"
+	SemVer = "v0.0.41"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 40
+	Patch = 41
 )
 
 // Version returns a pretty printed version information string.
