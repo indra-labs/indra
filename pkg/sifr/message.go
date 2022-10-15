66951a068fad1638b07cc4bd79fbf658c6872361
David Vennik <david@cybriq.systems>
2022-10-15 19:38:29 +0200

    Revising to minimal for using gotiny codec for fast serialization for the wire

--- pkg/sifr/message.go
+++ pkg/sifr/message.go
@@ -1,10 +1,7 @@
 package sifr
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
 	"crypto/rand"
-	"encoding/binary"
 	"sync"
 
 	"github.com/Indra-Labs/indra/pkg/schnorr"
@@ -64,98 +61,6 @@ type Outbound struct {
 	Data []byte
 }
 
-const MessageOverhead = schnorr.FingerprintLen + schnorr.PubkeyLen + NonceLen +
-	4 + 2 + schnorr.SigLen*3
-
-// Serialize converts a Message into the wire format, signs the payload before
-// encrypting and then signs the final packet.
-//
-// # Binary format
-//
-// # Field        - Size      - Description
-//
-// -----------------------------------------------------------------------------
-//
-// # To           - 8 bytes   - Fingerprint of public key of recipient used in with ECDH for cipher
-//
-// # From         - 32 bytes  - Public key of sender used with ECDH for cipher
-//
-// # MessageNonce - 12 bytes  - Cryptographically random nonce for message encryption
-//
-// # MessageSize  - 4 bytes   - Size of message (up to 4Gb)
-//
-// # Message      - variable  - Message is signed, signature appended, then encrypted with cipher
-//
-// # ExpireCount  - 2 bytes   - Number of expired public keys seen prior to dispatch of this message
-//
-// # Expired      - 8 bytes   - Fingerprint of expired public keys of recipient that have been seen
-//
-// …                          - repeats per ExpireCount
-//
-// Signature     - 64 bytes   - Schnorr Signature over entire message data (all previous fields) to prevent tampering
-func (m *Outbound) Serialize() (b []byte, e error) {
-	// Pre-allocate enough to ensure we have enough bytes for the serialised
-	// form of the message.
-	b = make([]byte, MessageOverhead+len(m.Data)+
-		schnorr.FingerprintLen*len(m.Expires))
-
-	var cursor int
-	// To
-	toFP := m.To.Fingerprint()
-	copy(b[cursor:cursor+len(toFP)], toFP[:])
-	cursor += len(toFP)
-	// From
-	fromKey := m.From.Pubkey().Serialize()
-	copy(b[cursor:cursor+len(fromKey)], fromKey[:])
-	cursor += len(fromKey)
-	// Nonce
-	copy(b[cursor:cursor+NonceLen], (*GetNonce())[:])
-	cursor += NonceLen
-	// Sign message
-	var sig *schnorr.Signature
-	if sig, e = m.From.Sign(schnorr.SHA256D(m.Data)); log.E.Chk(e) {
-		return
-	}
-	// MessageSize - message length is m.Data plus a signature
-	msgLen := len(m.Data) + schnorr.SigLen
-	binary.LittleEndian.PutUint32(b[cursor:cursor+4], uint32(msgLen))
-	cursor += 4
-	// Encrypt data
-	buf := make([]byte, msgLen)
-	// copy message into buffer with signature appended
-	copy(buf, m.Data)
-	copy(buf[len(m.Data):], (*sig.Serialize())[:])
-	// generate secret
-	secret := m.From.ECDH(m.To)
-	var ci cipher.Block
-	if ci, e = aes.NewCipher(secret); log.E.Chk(e) {
-		return
-	}
-	var gcm cipher.AEAD
-	if gcm, e = cipher.NewGCM(ci); log.E.Chk(e) {
-	}
-	gcm.Seal(buf[:0], b[cursor-NonceLen:cursor], buf, nil)
-	// copy encrypted message into buffer
-	copy(b[cursor:cursor+len(buf)], buf)
-	cursor += msgLen
-	// ExpireCount
-	binary.LittleEndian.PutUint16(b[cursor:cursor+2], uint16(len(m.Expires)))
-	cursor += 2
-	// Expired
-	for i := range m.Expires {
-		x := m.Expires[i].Fingerprint()
-		copy(b[cursor:cursor+schnorr.FingerprintLen], x[:])
-		cursor += schnorr.FingerprintLen
-	}
-	if sig, e = m.From.Sign(schnorr.SHA256D(b[:cursor])); log.E.Chk(e) {
-		return
-	}
-	copy(b[cursor:cursor+schnorr.SigLen], sig.Serialize()[:])
-	cursor += schnorr.SigLen
-	b = b[:cursor]
-	return
-}
-
 type Inbound struct {
 	// To is the fingerprint of the pubkey used in the ECDH key exchange.
 	To *schnorr.Fingerprint
@@ -167,9 +72,4 @@ type Inbound struct {
 	// Expires are the fingerprints of public keys that the correspondent
 	// can now discard as they will not be used again.
 	Expires []schnorr.Fingerprint
-}
-
-func (m *Inbound) Deserialize(b []byte) (e error) {
-
-	return
 }
