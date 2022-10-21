7ddcda7f2b4af3dd02d7b841109bf61f3abcf20f
David Vennik <david@cybriq.systems>
2022-10-21 16:38:45 +0200

    changed sign/recover test to use hash instead of 32 byte message

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "3fd6e96cae0217488cb487fd0b6ead486a6d9629"
+	ParentGitCommit = "8970626485288f02d3002d9270dbd4f90b75d75f"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-10-21T16:34:07+02:00"
+	BuildTime = "2022-10-21T16:38:45+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.61"
+	SemVer = "v0.0.62"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 61
+	Patch = 62
 )
 
 // Version returns a pretty printed version information string.
