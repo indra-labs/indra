f1f2001f0b81ac4f3ed5665b06166a197d08717f
David Vennik <david@cybriq.systems>
2022-10-21 11:47:55 +0200

    Fixing error in bumper template

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "89e8c0360007d4060dcd905c3811b8365e62ae4b"
+	ParentGitCommit = "d4e24c1bade5c8b0dd5acc2374a96b91392b1d59"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-10-21T11:46:30+02:00"
+	BuildTime = "2022-10-21T11:47:55+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.59"
+	SemVer = "v0.0.60"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 59
+	Patch = 60
 )
 
 // Version returns a pretty printed version information string.
@@ -31,7 +31,7 @@ func Version() string {
 		"\nRepository Information\n",
 		"\tGit repository: "+URL+"\n",
 		"\tBranch: "+GitRef+"\n",
-		"\ParentGitCommit: "+ParentGitCommit+"\n",
+		"\tParentGitCommit: "+ParentGitCommit+"\n",
 		"\tBuilt: "+BuildTime+"\n",
 		"\tSemVer: "+SemVer+"\n",
 	)
