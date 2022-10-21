1e17d0ef6119cf3474232f2b9ebfc6b225cbb587
David Vennik <david@cybriq.systems>
2022-10-21 16:53:38 +0200

    updating bumper to recompile itself

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "7ddcda7f2b4af3dd02d7b841109bf61f3abcf20f"
+	ParentGitCommit = "e96dd2c9ef53c5b2316d413bd275e4cf7c6cdbd1"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-10-21T16:48:48+02:00"
+	BuildTime = "2022-10-21T16:53:38+02:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.63"
+	SemVer = "v0.0.64"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 63
+	Patch = 64
 )
 
 // Version returns a pretty printed version information string.
