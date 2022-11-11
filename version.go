2616c08405d56b3e22cc3a583f6f310102829022
David Vennik <david@cybriq.systems>
2022-11-11 06:42:37 +0100

    Created packet frame protocol specification
    explanation of addresses and signers

--- version.go
+++ version.go
@@ -1,4 +1,4 @@
-package proc
+package indra
 
 import (
 	"fmt"
@@ -6,15 +6,15 @@ import (
 
 var (
 	// URL is the git URL for the repository.
-	URL = "github.com/Indra-Labs/indranet"
+	URL = "github.com/Indra-Labs/indra"
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "58afc076f1599275e82e0eb9af24c0a603d442af"
+	ParentGitCommit = "a930fe509ef45f604c40d4ddb0f454e52817244f"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-09-29T20:50:01+02:00"
+	BuildTime = "2022-11-11T06:42:37+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.38"
+	SemVer = "v0.0.87"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 38
+	Patch = 87
 )
 
 // Version returns a pretty printed version information string.
@@ -31,7 +31,7 @@ func Version() string {
 		"\nRepository Information\n",
 		"\tGit repository: "+URL+"\n",
 		"\tBranch: "+GitRef+"\n",
-		"\tPacethGitCommit: "+ParentGitCommit+"\n",
+		"\tParentGitCommit: "+ParentGitCommit+"\n",
 		"\tBuilt: "+BuildTime+"\n",
 		"\tSemVer: "+SemVer+"\n",
 	)
