aa9e85e71c1a4258035fa3c95b05d50f7ade59e6
David Vennik <david@cybriq.systems>
2022-11-14 08:45:04 +0100

    Updated packet frame spec to match implementation

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "a930fe509ef45f604c40d4ddb0f454e52817244f"
+	ParentGitCommit = "1b38e6cfa5dada1d8de677c20a50a2b41b1407bd"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-11-11T06:42:37+01:00"
+	BuildTime = "2022-11-14T08:45:04+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.87"
+	SemVer = "v0.0.95"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 87
+	Patch = 95
 )
 
 // Version returns a pretty printed version information string.
