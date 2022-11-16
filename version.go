e5f30ff8e16bf78f522fd43577ffbe3824c08db2
David Vennik <david@cybriq.systems>
2022-11-16 18:19:17 +0100

    Added node model

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "38df8478ba01f8d2b68cd945d782f0b5186b1f46"
+	ParentGitCommit = "342ab981b388031126310c9860ed70c38798740a"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-11-16T11:00:29+01:00"
+	BuildTime = "2022-11-16T18:19:17+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.99"
+	SemVer = "v0.0.100"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 99
+	Patch = 100
 )
 
 // Version returns a pretty printed version information string.
