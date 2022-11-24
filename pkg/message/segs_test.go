78679a2ca22a633c29a94490b75f9bb409edc0e1
David Vennik <david@cybriq.systems>
2022-11-24 09:02:05 +0100

    Removed return key to generalise the message encoding for message subtypes

--- pkg/message/segs_test.go
+++ pkg/message/segs_test.go
@@ -8,34 +8,31 @@ import (
 var Expected = []string{
 	`
 	Segments{
-		Segment{ DStart: 0, DEnd: 192, PEnd: 256, SLen: 123, Last: 123},
-		Segment{ DStart: 256, DEnd: 448, PEnd: 512, SLen: 123, Last: 123},
-		Segment{ DStart: 512, DEnd: 704, PEnd: 768, SLen: 123, Last: 123},
-		Segment{ DStart: 768, DEnd: 960, PEnd: 1024, SLen: 123, Last: 123},
-		Segment{ DStart: 1024, DEnd: 1216, PEnd: 1280, SLen: 123, Last: 123},
-		Segment{ DStart: 1280, DEnd: 1472, PEnd: 1536, SLen: 123, Last: 123},
-		Segment{ DStart: 1536, DEnd: 1728, PEnd: 1792, SLen: 123, Last: 123},
-		Segment{ DStart: 1792, DEnd: 1984, PEnd: 2048, SLen: 123, Last: 123},
-		Segment{ DStart: 2048, DEnd: 2240, PEnd: 2304, SLen: 123, Last: 123},
-		Segment{ DStart: 2304, DEnd: 2496, PEnd: 2560, SLen: 123, Last: 123},
-		Segment{ DStart: 2560, DEnd: 2752, PEnd: 2816, SLen: 123, Last: 123},
-		Segment{ DStart: 2816, DEnd: 2837, PEnd: 2844, SLen: 123, Last: 19},
+		Segment{ DStart: 0, DEnd: 192, PEnd: 256, SLen: 156, Last: 156},
+		Segment{ DStart: 256, DEnd: 448, PEnd: 512, SLen: 156, Last: 156},
+		Segment{ DStart: 512, DEnd: 704, PEnd: 768, SLen: 156, Last: 156},
+		Segment{ DStart: 768, DEnd: 960, PEnd: 1024, SLen: 156, Last: 156},
+		Segment{ DStart: 1024, DEnd: 1216, PEnd: 1280, SLen: 156, Last: 156},
+		Segment{ DStart: 1280, DEnd: 1472, PEnd: 1536, SLen: 156, Last: 156},
+		Segment{ DStart: 1536, DEnd: 1728, PEnd: 1792, SLen: 156, Last: 156},
+		Segment{ DStart: 1792, DEnd: 1984, PEnd: 2048, SLen: 156, Last: 156},
+		Segment{ DStart: 2048, DEnd: 2194, PEnd: 2242, SLen: 156, Last: 19},
 	}
 `,
 	`
 	Segments{
-		Segment{ DStart: 0, DEnd: 133, PEnd: 133, SLen: 3963, Last: 1172},
+		Segment{ DStart: 0, DEnd: 132, PEnd: 132, SLen: 3996, Last: 812},
 	}
 `,
 	`
 	Segments{
-		Segment{ DStart: 0, DEnd: 128, PEnd: 256, SLen: 3963, Last: 3963},
-		Segment{ DStart: 256, DEnd: 261, PEnd: 266, SLen: 3963, Last: 1172},
+		Segment{ DStart: 0, DEnd: 128, PEnd: 256, SLen: 3996, Last: 3996},
+		Segment{ DStart: 256, DEnd: 260, PEnd: 264, SLen: 3996, Last: 812},
 	}
 `,
 	`
 	Segments{
-		Segment{ DStart: 0, DEnd: 67, PEnd: 67, SLen: 3963, Last: 586},
+		Segment{ DStart: 0, DEnd: 66, PEnd: 66, SLen: 3996, Last: 2404},
 	}
 `,
 }
