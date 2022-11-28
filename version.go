d5865fe9bdb73172abd675977444eabffef94bcb
David Vennik <david@cybriq.systems>
2022-11-28 15:38:42 +0100

    fixed incorrect function call in node/relay.go

--- version.go
+++ version.go
@@ -13,11 +13,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "87419759ce0848b45173037b28049dd0f9a93ce6"
+	ParentGitCommit = "59b28d573d59eaa8733cc58befe31348b8a30758"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-11-28T15:32:02+01:00"
+	BuildTime = "2022-11-28T15:38:42+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.133"
+	SemVer = "v0.0.134"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 133
+	Patch = 134
 )
 
 // Version returns a pretty printed version information string.
