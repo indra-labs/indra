aefd700daeb9863571bab7cd17a293ad52cb1af8
David Vennik <david@cybriq.systems>
2022-11-28 16:10:56 +0100

    Updated to subtly coloured logo version

--- version.go
+++ version.go
@@ -13,11 +13,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "59b28d573d59eaa8733cc58befe31348b8a30758"
+	ParentGitCommit = "cdb9c81832ebc38882ac9d543baaaa2c35cd69e8"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-11-28T15:38:42+01:00"
+	BuildTime = "2022-11-28T16:10:55+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.134"
+	SemVer = "v0.0.136"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 134
+	Patch = 136
 )
 
 // Version returns a pretty printed version information string.
