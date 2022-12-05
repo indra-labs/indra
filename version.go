7380bcdc1dbc4335e26753bb5a2b1928f8e4beae
David Vennik <david@cybriq.systems>
2022-12-05 07:40:07 +0100

    Clarified exit message formatting

--- version.go
+++ version.go
@@ -13,11 +13,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "5d1c67824760812269ab8ffc7641b7728b6ad597"
+	ParentGitCommit = "f2ab72cde669d632f92a37b9068ae41067e1714f"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-11-28T16:21:42+01:00"
+	BuildTime = "2022-12-05T07:40:07+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.138"
+	SemVer = "v0.0.147"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 138
+	Patch = 147
 )
 
 // Version returns a pretty printed version information string.
