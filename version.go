205a6a064ec2d2e0ba54ee1eaa4d656b3c90cade
David Vennik <david@cybriq.systems>
2022-12-07 14:27:47 +0100

    added arrows to hex diagrams

--- version.go
+++ version.go
@@ -13,11 +13,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "40d91b526af94a9cff39458bdb7c69469b21bf18"
+	ParentGitCommit = "f8de7ff6af12917c07009d29cfc90f77e43bca06"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-07T14:03:56+01:00"
+	BuildTime = "2022-12-07T14:27:47+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.157"
+	SemVer = "v0.0.158"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 157
+	Patch = 158
 )
 
 // Version returns a pretty printed version information string.
