c8effb8381f646014355a486f2ab920d15d81c05
David Vennik <david@cybriq.systems>
2022-12-07 16:47:54 +0100

    Add more text about path configuration, rendezvous and so on

--- version.go
+++ version.go
@@ -13,11 +13,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "205a6a064ec2d2e0ba54ee1eaa4d656b3c90cade"
+	ParentGitCommit = "eecf3ccc8668d0378b0bd31d7986fd01c455ad03"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-07T14:32:38+01:00"
+	BuildTime = "2022-12-07T16:47:54+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.159"
+	SemVer = "v0.0.160"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 159
+	Patch = 160
 )
 
 // Version returns a pretty printed version information string.
