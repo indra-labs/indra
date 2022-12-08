6e69645878162d42aa1e79ea2dc0e3d076fd99b3
David Vennik <david@cybriq.systems>
2022-12-08 13:28:16 +0100

    Added relay to relay traffic description and return message structure

--- version.go
+++ version.go
@@ -13,11 +13,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "eecf3ccc8668d0378b0bd31d7986fd01c455ad03"
+	ParentGitCommit = "57112689048c2adefde66958e5a50c19e0926331"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-07T16:47:54+01:00"
+	BuildTime = "2022-12-08T13:28:16+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.160"
+	SemVer = "v0.0.161"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 160
+	Patch = 161
 )
 
 // Version returns a pretty printed version information string.
