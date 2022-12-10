94f0b3e0f8a6c2fd2ae4dd3db9c5745ab8571532
David Vennik <david@cybriq.systems>
2022-12-10 10:34:32 +0100

    Added message type byte and changed ECDH parameter to a pubkey instead of sig

--- version.go
+++ version.go
@@ -13,11 +13,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "f0baa3a4880e7f90b83a6766ef593193a44aa5b2"
+	ParentGitCommit = "3d090b9567be9859405b43a7d2be9d93675a5c52"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-10T10:22:12+01:00"
+	BuildTime = "2022-12-10T10:34:32+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.167"
+	SemVer = "v0.0.168"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 167
+	Patch = 168
 )
 
 // Version returns a pretty printed version information string.
