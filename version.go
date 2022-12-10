53a68f2c7ce687303528c026c1fa14ed3089ecd6
David Vennik <david@cybriq.systems>
2022-12-10 11:27:56 +0100

    Clarified return message protocol including free tier forward circuit ciphers

--- version.go
+++ version.go
@@ -13,11 +13,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "11b8b67aa4ee4f39159c77053d3199e9113b569b"
+	ParentGitCommit = "b545a33f88f32ca87ab7fd5d98acc2ea965eb158"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-10T11:17:09+01:00"
+	BuildTime = "2022-12-10T11:27:56+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.171"
+	SemVer = "v0.0.172"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 171
+	Patch = 172
 )
 
 // Version returns a pretty printed version information string.
