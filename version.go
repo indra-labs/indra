5c1f1576c659aaa80aef70aad02d4a01e15b7030
David Vennik <david@cybriq.systems>
2022-12-10 09:03:35 +0100

    Remove composition, add fast XOR, added notes about return secondary keys

--- version.go
+++ version.go
@@ -13,11 +13,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "dbe8d115254dc3f5f9857685b56935fe4eb2f51f"
+	ParentGitCommit = "1c947633bcf8c49fff106151da27e45c4df635c2"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-09T18:22:47+01:00"
+	BuildTime = "2022-12-10T09:03:35+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.164"
+	SemVer = "v0.0.165"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 164
+	Patch = 165
 )
 
 // Version returns a pretty printed version information string.
