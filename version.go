3d436e9c7c6b9c1fb430e9cb1f0f28fc9aaea0cf
David Vennik <david@cybriq.systems>
2022-12-11 14:18:08 +0100

    Stubbed in wire message types and interface

--- version.go
+++ version.go
@@ -13,11 +13,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "9030a2f70411a2df3229a26e8114410cacbb35e3"
+	ParentGitCommit = "70ca966817cddb2e6916141b330c8764187c2b32"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-10T15:24:46+01:00"
+	BuildTime = "2022-12-11T14:18:08+01:00"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.177"
+	SemVer = "v0.0.178"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -25,7 +25,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 177
+	Patch = 178
 )
 
 // Version returns a pretty printed version information string.
