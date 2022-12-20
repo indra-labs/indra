61bd600f319bb33fcb56488ca715a812e36c66a0
David Vennik <david@cybriq.systems>
2022-12-20 11:11:58 +0000

    Created SendReturn onion layering scheme and clarified Header vs Payload keys for Return

--- pkg/wire/onion.go
+++ pkg/wire/onion.go
@@ -2,6 +2,7 @@ package wire
 
 import (
 	"github.com/Indra-Labs/indra/pkg/key/address"
+	"github.com/Indra-Labs/indra/pkg/key/prv"
 	"github.com/Indra-Labs/indra/pkg/key/signer"
 	"github.com/Indra-Labs/indra/pkg/node"
 	"github.com/Indra-Labs/indra/pkg/nonce"
@@ -27,6 +28,37 @@ func Ping(ciph sha256.Hash, id nonce.ID, nodes [3]node.Node,
 		Message(address.FromPubKey(nodes[1].Forward), set.Next()).
 		Forward(nodes[1].IP).
 		Message(address.FromPubKey(nodes[2].Forward), set.Next()).
+		Forward(nodes[2].IP).
+		Confirmation(ciph, id).
+		Assemble()
+}
+
+// SendReturn provides a pair of private keys that will be used to generate the
+// Purchase header bytes and to generate the ciphers provided in the Purchase
+// message to encrypt the Session that is returned.
+//
+// The Header key, its cloaked public key counterpart used in the To field of
+// the Purchase message preformed header bytes, but the Ciphers provided in the
+// Purchase message, for encrypting the Session to be returned, uses the Payload
+// key, along with the public key found in the encrypted layer of the header for
+// the Return relay.
+//
+// This message's last layer is a Confirmation, which allows the client to know
+// that the key was successfully delivered to the Return relays that will be
+// used in the Purchase.
+func SendReturn(id nonce.ID, ciph sha256.Hash, hdr, pld *prv.Key,
+	nodes [5]node.Node, set signer.KeySet) Onion {
+
+	return OnionSkins{}.
+		Message(address.FromPubKey(nodes[0].Forward), set.Next()).
+		Forward(nodes[0].IP).
+		Message(address.FromPubKey(nodes[1].Forward), set.Next()).
+		Forward(nodes[1].IP).
+		Message(address.FromPubKey(nodes[2].Forward), set.Next()).
+		Cipher(hdr, pld).
+		Message(address.FromPubKey(nodes[3].Forward), set.Next()).
+		Forward(nodes[1].IP).
+		Message(address.FromPubKey(nodes[4].Forward), set.Next()).
 		Forward(nodes[2].IP).
 		Confirmation(ciph, id).
 		Assemble()
