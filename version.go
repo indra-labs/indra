55b15d892676ea02a3e7e43b14728d73725edb1c
David Vennik <david@cybriq.systems>
2022-12-23 19:44:45 +0000

    Bumping proc version

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "89f4a51a85574c75aa0fa27b7b2d13381e9ee6b2"
+	ParentGitCommit = "9f843d9911e185ceb2f0bbf7fcbaa0cf311f494d"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-23T18:31:03Z"
+	BuildTime = "2022-12-23T19:44:45Z"
 	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.221"
+	SemVer = "v0.0.222"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 221
+	Patch = 222
 )
 
 // Version returns a pretty printed version information string.
