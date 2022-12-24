63097944d0d9429ea055c28ca481ab546b06f800
Colin Lyons <colin@tercet.io>
2022-12-24 15:55:58 +0000

    Adding affirmation to serve.

--- cmd/indra/handlers.go
+++ cmd/indra/handlers.go
@@ -19,6 +19,9 @@ var versionHandler = func(c *cmds.Command, args []string) error {
 
 var serveHandler = func(c *cmds.Command, args []string) error {
 
+	log.I.Ln("-- “Far away in the heavenly abode of the great god indra, there is a wonderful net which has been hung by some cunning artificer in such a manner that it stretches out indefinitely in all directions.”")
+	log.I.Ln("-- “In accordance with the extravagant tastes of deities, the artificer has hung a single glittering jewel at the net’s every node, and since the net itself is infinite in dimension, the jewels are infinite in number. There hang the jewels, glittering like stars of the first magnitude, a wonderful sight to behold.”")
+	log.I.Ln("-- “If we now arbitrarily select one of these jewels for inspection and look closely at it, we will discover that in its polished surface there are reflected all the other jewels in the net, infinite in number. Not only that, but each of the jewels reflected in this one jewel is also reflecting all the other jewels, so that the process of reflection is infinite.”")
 
 	log.I.Ln("running serve.")
 
