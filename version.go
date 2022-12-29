e062098487e59d1c4924bbde8f29e43077e2c8a3
Colin Lyons <colin@tercet.io>
2022-12-29 14:43:34 +0000

    lots of little pieces of refactoring.

--- version.go
+++ version.go
@@ -8,21 +8,21 @@ var (
 	// URL is the git URL for the repository.
 	URL = "github.com/Indra-Labs/indra"
 	// GitRef is the gitref, as in refs/heads/branchname.
-	GitRef = "refs/heads/protocol"
+	GitRef = "refs/heads/ind-bootstrap"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "83a692da2d96ef804e799e802d6815b07aa66c77"
+	ParentGitCommit = "878a6ec79d65b75c9dbc05274bee6eba923d2134"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-29T12:06:37Z"
+	BuildTime = "2022-12-29T14:43:34Z"
 	// SemVer lists the (latest) git tag on the release.
-	SemVer = "v0.0.259"
+	SemVer = "v0.1.0"
 	// PathBase is the path base returned from runtime caller.
-	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
+	PathBase = "/home/lyo/Seafile/Git/indra-labs/indra/"
 	// Major is the major number from the tag.
 	Major = 0
 	// Minor is the minor number from the tag.
-	Minor = 0
+	Minor = 1
 	// Patch is the patch version number from the tag.
-	Patch = 259
+	Patch = 0
 )
 
 // Version returns a pretty printed version information string.
