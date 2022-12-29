ed542f0b9ced648125640f1f8c4f5f4fe55cd677
David Vennik <david@cybriq.systems>
2022-12-29 08:07:49 +0000

    fixing workflow command

--- version.go
+++ version.go
@@ -8,21 +8,21 @@ var (
 	// URL is the git URL for the repository.
 	URL = "github.com/Indra-Labs/indra"
 	// GitRef is the gitref, as in refs/heads/branchname.
-	GitRef = "refs/heads/ind-bootstrap"
+	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "2dc3027cf28b04b52b8bca3839a96267122f8b92"
+	ParentGitCommit = "090ea9584cb7987911cccd390e5470e966938378"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-29T02:49:15Z"
-	// SemVer lists the (latest) git tag on the release.
-	SemVer = "v0.0.255"
+	BuildTime = "2022-12-29T08:07:49Z"
+	// SemVer lists the (latest) git tag on the build.
+	SemVer = "v0.0.256"
 	// PathBase is the path base returned from runtime caller.
-	PathBase = "/home/lyo/Seafile/Git/indra-labs/indra/"
+	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
 	Major = 0
 	// Minor is the minor number from the tag.
 	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 255
+	Patch = 256
 )
 
 // Version returns a pretty printed version information string.
