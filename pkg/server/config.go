34b3bcc60a2e77175e6959ab2bdf9918d22b1aac
Colin Lyons <colin@tercet.io>
2022-12-30 15:42:26 +0000

    debugging environment vars from proc.

--- pkg/server/config.go
+++ pkg/server/config.go
@@ -4,8 +4,8 @@ import "github.com/multiformats/go-multiaddr"
 
 var DefaultServerConfig = Config{
 
-	ListenAddresses: []multiaddr.Multiaddr{NewMultiAddrForced("/ip4/127.0.0.1/tcp/8337")},
 	SeedAddresses:   []multiaddr.Multiaddr{},
+	ListenAddresses: []multiaddr.Multiaddr{},
 }
 
 func NewMultiAddrForced(addr string) multiaddr.Multiaddr {
