bddac4e0e886e8567844b5156ef630f915e5b124
Colin Lyons <colin@tercet.io>
2022-12-31 15:18:29 +0000

    bumping version

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/ind-bootstrap"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "1480ad99a31d57e87ca5109500900797c0e8dfa3"
+	ParentGitCommit = "de0d9d356f80f91633a7173f5f2fabb5737b48b2"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-30T17:14:07Z"
+	BuildTime = "2022-12-31T15:18:29Z"
 	// SemVer lists the (latest) git tag on the release.
-	SemVer = "v0.1.0"
+	SemVer = "v0.1.2"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/lyo/Seafile/Git/indra-labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 1
 	// Patch is the patch version number from the tag.
-	Patch = 0
+	Patch = 2
 )
 
 // Version returns a pretty printed version information string.
