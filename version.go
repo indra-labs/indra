54993426b03b90624299164cc0a78ddaa08e0da0
David Vennik <david@cybriq.systems>
2023-01-04 14:27:44 +0000

    Fixed errors in layer tests

--- version.go
+++ version.go
@@ -8,13 +8,13 @@ var (
 	// URL is the git URL for the repository.
 	URL = "github.com/Indra-Labs/indra"
 	// GitRef is the gitref, as in refs/heads/branchname.
-	GitRef = "refs/heads/protocol"
+	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "88b43a63c852ac28368579723abac54f413141de"
+	ParentGitCommit = "e4ec4b26b403ced5ad73e2a9bf93b9b740672f2b"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2023-01-04T14:26:34Z"
+	BuildTime = "2023-01-04T14:27:44Z"
 	// SemVer lists the (latest) git tag on the release.
-	SemVer = "v0.1.3"
+	SemVer = "v0.1.4"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 1
 	// Patch is the patch version number from the tag.
-	Patch = 3
+	Patch = 4
 )
 
 // Version returns a pretty printed version information string.
