7d027bce93c0bad0031d004218f39fcca2c2fe3a
David Vennik <david@cybriq.systems>
2023-01-06 13:34:32 +0000

    migrating to lower case org name

--- version.go
+++ version.go
@@ -1,6 +1,3 @@
-// Package indra is the root level package for Indranet, a low latency, 
-// Lightning Network monetised distributed VPN protocol designed for providing
-// strong anonymity to valuable internet traffic.
 package indra
 
 import (
@@ -9,23 +6,23 @@ import (
 
 var (
 	// URL is the git URL for the repository.
-	URL = "github.com/indra-labs/indra"
+	URL = "github.com/Indra-Labs/indra"
 	// GitRef is the gitref, as in refs/heads/branchname.
-	GitRef = "refs/heads/main"
+	GitRef = "refs/heads/protocol"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "b7b5e1872cae115dd7dbda517d9eaacb30ad4d66"
+	ParentGitCommit = "540f384502f3fc5cf753aee2c78743cf52cc215b"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2022-12-11T21:50:35+01:00"
-	// SemVer lists the (latest) git tag on the build.
-	SemVer = "v0.0.180"
+	BuildTime = "2023-01-06T13:34:32Z"
+	// SemVer lists the (latest) git tag on the release.
+	SemVer = "v0.1.4"
 	// PathBase is the path base returned from runtime caller.
-	PathBase = "/home/loki/src/github.com/Indra-Labs/indra/"
+	PathBase = "/home/loki/src/github.com/indra-labs/indra/"
 	// Major is the major number from the tag.
 	Major = 0
 	// Minor is the minor number from the tag.
-	Minor = 0
+	Minor = 1
 	// Patch is the patch version number from the tag.
-	Patch = 180
+	Patch = 4
 )
 
 // Version returns a pretty printed version information string.
