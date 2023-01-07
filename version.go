a3475db7b591f9abc45559da4dc8986af7e348ee
David Vennik <david@cybriq.systems>
2023-01-07 17:48:04 +0000

    Merging latest from protocol and actions now for active branches

--- version.go
+++ version.go
@@ -8,13 +8,13 @@ var (
 	// URL is the git URL for the repository.
 	URL = "github.com/indra-labs/indra"
 	// GitRef is the gitref, as in refs/heads/branchname.
-	GitRef = "refs/heads/protocol"
+	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "cd0c99e5869a8180ca762bba1468298f4ad9cbd9"
+	ParentGitCommit = "84d9247615c53692d751769fec43fc654f54814b"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2023-01-07T17:46:08Z"
+	BuildTime = "2023-01-07T17:48:04Z"
 	// SemVer lists the (latest) git tag on the release.
-	SemVer = "v0.1.4"
+	SemVer = "v0.1.5"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/indra-labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 1
 	// Patch is the patch version number from the tag.
-	Patch = 4
+	Patch = 5
 )
 
 // Version returns a pretty printed version information string.
