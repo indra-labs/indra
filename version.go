6c85cf32649418b421ddc201c7e194bfd255d3b5
David Vennik <david@cybriq.systems>
2023-01-09 14:44:56 +0000

    Add section about mixnet vulnerabilities and Indranet

--- version.go
+++ version.go
@@ -6,15 +6,15 @@ import (
 
 var (
 	// URL is the git URL for the repository.
-	URL = "github.com/Indra-Labs/indra"
+	URL = "github.com/indra-labs/indra"
 	// GitRef is the gitref, as in refs/heads/branchname.
-	GitRef = "refs/heads/protocol"
+	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "540f384502f3fc5cf753aee2c78743cf52cc215b"
+	ParentGitCommit = "d99b54fdfc80cd4694e4c4d8495dc5a48a93169b"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2023-01-06T13:34:32Z"
+	BuildTime = "2023-01-09T14:44:56Z"
 	// SemVer lists the (latest) git tag on the release.
-	SemVer = "v0.1.4"
+	SemVer = "v0.1.7"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/indra-labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 1
 	// Patch is the patch version number from the tag.
-	Patch = 4
+	Patch = 7
 )
 
 // Version returns a pretty printed version information string.
