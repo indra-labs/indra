7657f425293be95b5bd13b9fa626c5f8ab52ce56
David Vennik <david@cybriq.systems>
2023-01-09 00:04:27 +0000

    Fixed logging printing and command struct walking

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "84d9247615c53692d751769fec43fc654f54814b"
+	ParentGitCommit = "4d6d252323febf8bb0d3f0783e02b762ca398e2d"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2023-01-07T17:48:04Z"
+	BuildTime = "2023-01-09T00:04:27Z"
 	// SemVer lists the (latest) git tag on the release.
-	SemVer = "v0.1.5"
+	SemVer = "v0.1.6"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/indra-labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 1
 	// Patch is the patch version number from the tag.
-	Patch = 5
+	Patch = 6
 )
 
 // Version returns a pretty printed version information string.
