4384b6525f83f3faaf577f107552a9ea381be09c
David Vennik <david@cybriq.systems>
2023-01-17 11:16:21 +0000

    big improvements in repo layout

--- version.go
+++ version.go
@@ -10,11 +10,11 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "6669de9359063047d5a03bf0e50084a35628a30a"
+	ParentGitCommit = "a6f9c9bfa33c2576d66408ac2e1f1785eaa2eff3"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2023-01-16T21:18:00Z"
+	BuildTime = "2023-01-17T11:16:21Z"
 	// SemVer lists the (latest) git tag on the release.
-	SemVer = "v0.1.8"
+	SemVer = "v0.1.9"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/home/loki/src/github.com/indra-labs/indra/"
 	// Major is the major number from the tag.
@@ -22,7 +22,7 @@ var (
 	// Minor is the minor number from the tag.
 	Minor = 1
 	// Patch is the patch version number from the tag.
-	Patch = 8
+	Patch = 9
 )
 
 // Version returns a pretty printed version information string.
