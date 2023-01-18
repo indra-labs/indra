ca90a3760584e0c9e1f0b5585234aec436377127
Colin Lyons <colin@tercet.io>
2023-01-18 16:43:09 +0000

    adding a dedicated file for source configurations.

---
+++ cmd/docker/release/sources.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/docker/docker/api/types"
+	"github.com/indra-labs/indra/pkg/docker"
+)
+
+var sourceConfigurations = []docker.BuildConfiguration{
+	docker.BuildConfiguration{
+		Name:            defaultRepositoryName + "/" + "btcd-source",
+		ContextFilePath: "/tmp/btcd-source.tar",
+		BuildOpts: types.ImageBuildOptions{
+			Dockerfile: "docker/btcd/source/official.Dockerfile",
+			Tags: []string{
+				"v0.23.3",
+			},
+			BuildArgs: map[string]*string{
+				"sourcing_image":            strPtr(defaultBuildContainer),
+				"source_release_url_prefix": strPtr("https://github.com/btcsuite/btcd/releases/download"),
+				"source_version":            strPtr("v0.23.3"),
+			},
+			SuppressOutput: false,
+			Remove:         false,
+			ForceRemove:    false,
+			PullParent:     false,
+		},
+	},
+	docker.BuildConfiguration{
+		Name:            defaultRepositoryName + "/" + "lnd-source",
+		ContextFilePath: "/tmp/lnd-source.tar",
+		BuildOpts: types.ImageBuildOptions{
+			Dockerfile: "docker/lnd/source/official.Dockerfile",
+			Tags: []string{
+				"v0.15.5-beta",
+			},
+			BuildArgs: map[string]*string{
+				"sourcing_image":            strPtr(defaultBuildContainer),
+				"source_release_url_prefix": strPtr("https://github.com/lightningnetwork/lnd/releases/download"),
+				"source_version":            strPtr("v0.15.5-beta"),
+			},
+			SuppressOutput: false,
+			Remove:         false,
+			ForceRemove:    false,
+			PullParent:     false,
+		},
+	},
+	//docker.BuildConfiguration{
+	//	Name:            defaultRepositoryName + "/" + "indra-source",
+	//	ContextFilePath: "/tmp/indra-source.tar",
+	//	BuildOpts: types.ImageBuildOptions{
+	//		Dockerfile: "docker/indra/source/official.Dockerfile",
+	//		Tags: []string{
+	//			"v0.1.9",
+	//		},
+	//		BuildArgs: map[string]*string{
+	//			"sourcing_image":            strPtr(defaultBuildContainer),
+	//			"source_release_url_prefix": strPtr("https://github.com/indra-labs/indra/releases/download"),
+	//			"source_version":            strPtr("v0.1.9"),
+	//		},
+	//		SuppressOutput: false,
+	//		Remove:         false,
+	//		ForceRemove:    false,
+	//		PullParent:     false,
+	//	},
+	//},
+}
