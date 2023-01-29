73b84147dea4d8ddd89917ed291ad1cdbd7f663f
херетик <heretik@indra.org>
2023-01-29 10:19:38 +0000

    bumping version

--- version.go
+++ version.go
@@ -10,19 +10,19 @@ var (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/main"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "94f401d5e318c151c073bf81cf7685cbf3867347"
+	ParentGitCommit = "170f3c64a32cb6237e3ee2336890215e11c4a356"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2023-01-27T15:41:43Z"
+	BuildTime = "2023-01-29T10:19:38Z"
 	// SemVer lists the (latest) git tag on the release.
-	SemVer = "v0.1.12"
+	SemVer = "v0.0.1"
 	// PathBase is the path base returned from runtime caller.
 	PathBase = "/opt/indra-labs/indra/"
 	// Major is the major number from the tag.
 	Major = 0
 	// Minor is the minor number from the tag.
-	Minor = 1
+	Minor = 0
 	// Patch is the patch version number from the tag.
-	Patch = 12
+	Patch = 1
 )
 
 // Version returns a pretty printed version information string.
