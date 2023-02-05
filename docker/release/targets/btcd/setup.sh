436a2da9e0d8193987f9920617247c5dec3c7451
greg stone <greg.stone@indra.org>
2023-02-05 19:26:39 +0000

    adding some documentation.

--- docker/release/targets/btcd/setup.sh
+++ docker/release/targets/btcd/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Remove existing containers
 docker rm btcd-btcd-1 btcd-btcctl-1 btcd-btcwallet-1 2>/dev/null
 
+# Remove existing volumes
 docker volume rm btcd_config btcd_data btcd_btcwallet_config btcd_btcwallet_data 2>/dev/null
 
+# Setup an rpc key/cert for the btcwallet daemon
 docker run --rm -it \
   --volume=btcd_btcwallet_config:/etc/btcwallet \
   --entrypoint="/bin/gencerts" \
@@ -11,9 +14,15 @@ docker run --rm -it \
   indralabs/btcd-multi-arch:linux-amd64-dev \
     --directory=/etc/btcwallet -H 172.16.42.3 -f
 
+# Create a new wallet
 docker run --rm -it \
   --volume=btcd_btcwallet_config:/etc/btcwallet \
   --volume=btcd_btcwallet_data:/var/btcwallet \
   indralabs/btcwallet-multi-arch:linux-amd64-dev \
-    --simnet --create
+    --simnet --createtemp
 
+#docker run --rm -it \
+#  --volume=btcd_btcwallet_config:/etc/btcwallet \
+#  --volume=btcd_btcwallet_data:/var/btcwallet \
+#  indralabs/btcwallet-multi-arch:linux-amd64-dev \
+#    --simnet importprivkey FuarsNCxniX277tBYt1BDGPB6cRTUfeEhUBXNAjrg3cdsWZTNcPj
