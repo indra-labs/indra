6b9827e5f8b9018eeb317f690a5a434482c36816
херетик <heretik@indra.org>
2023-04-02 09:44:14 +0100

    fixed test fail from name change

--- pkg/engine/segcalc_test.go
+++ pkg/engine/segcalc_test.go
@@ -7,31 +7,31 @@ import (
 
 var Expected = []string{
 	`
-	SpliceSegments{
-		PacketSegment{ DStart: 0, DEnd: 192, PEnd: 256, SLen: 195, ID: 195},
-		PacketSegment{ DStart: 256, DEnd: 448, PEnd: 512, SLen: 195, ID: 195},
-		PacketSegment{ DStart: 512, DEnd: 704, PEnd: 768, SLen: 195, ID: 195},
-		PacketSegment{ DStart: 768, DEnd: 960, PEnd: 1024, SLen: 195, ID: 195},
-		PacketSegment{ DStart: 1024, DEnd: 1216, PEnd: 1280, SLen: 195, ID: 195},
-		PacketSegment{ DStart: 1280, DEnd: 1472, PEnd: 1536, SLen: 195, ID: 195},
-		PacketSegment{ DStart: 1536, DEnd: 1728, PEnd: 1792, SLen: 195, ID: 195},
-		PacketSegment{ DStart: 1792, DEnd: 1793, PEnd: 1794, SLen: 195, ID: 175},
+	Segments{
+		Segment{ DStart: 0, DEnd: 192, PEnd: 256, SLen: 195, ID: 195},
+		Segment{ DStart: 256, DEnd: 448, PEnd: 512, SLen: 195, ID: 195},
+		Segment{ DStart: 512, DEnd: 704, PEnd: 768, SLen: 195, ID: 195},
+		Segment{ DStart: 768, DEnd: 960, PEnd: 1024, SLen: 195, ID: 195},
+		Segment{ DStart: 1024, DEnd: 1216, PEnd: 1280, SLen: 195, ID: 195},
+		Segment{ DStart: 1280, DEnd: 1472, PEnd: 1536, SLen: 195, ID: 195},
+		Segment{ DStart: 1536, DEnd: 1728, PEnd: 1792, SLen: 195, ID: 195},
+		Segment{ DStart: 1792, DEnd: 1793, PEnd: 1794, SLen: 195, ID: 175},
 	}
 `,
 	`
-	SpliceSegments{
-		PacketSegment{ DStart: 0, DEnd: 130, PEnd: 130, SLen: 4035, ID: 3773},
+	Segments{
+		Segment{ DStart: 0, DEnd: 130, PEnd: 130, SLen: 4035, ID: 3773},
 	}
 `,
 	`
-	SpliceSegments{
-		PacketSegment{ DStart: 0, DEnd: 128, PEnd: 256, SLen: 4035, ID: 4035},
-		PacketSegment{ DStart: 256, DEnd: 258, PEnd: 260, SLen: 4035, ID: 3773},
+	Segments{
+		Segment{ DStart: 0, DEnd: 128, PEnd: 256, SLen: 4035, ID: 4035},
+		Segment{ DStart: 256, DEnd: 258, PEnd: 260, SLen: 4035, ID: 3773},
 	}
 `,
 	`
-	SpliceSegments{
-		PacketSegment{ DStart: 0, DEnd: 65, PEnd: 65, SLen: 4035, ID: 3904},
+	Segments{
+		Segment{ DStart: 0, DEnd: 65, PEnd: 65, SLen: 4035, ID: 3904},
 	}
 `,
 }
