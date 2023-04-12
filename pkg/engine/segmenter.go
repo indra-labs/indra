61f7b8f00d23f808e6f790b778807c1072521085
херетик <heretik@indra.org>
2023-04-12 07:05:41 +0100

    added hash to tx/rx record and document the fields

--- pkg/engine/segmenter.go
+++ pkg/engine/segmenter.go
@@ -6,56 +6,65 @@ import (
 	
 	"git-indra.lan/indra-labs/indra/pkg/crypto"
 	"git-indra.lan/indra-labs/indra/pkg/crypto/nonce"
+	"git-indra.lan/indra-labs/indra/pkg/crypto/sha256"
 )
 
-// TxRecord is the details of a send operation in progress.
-// This is used with the data received in the acknowledgement,
-// which is a completed RxRecord..
+// TxRecord is the details of a send operation in progress. This is used with
+// the data received in the acknowledgement, which is a completed RxRecord..
 type TxRecord struct {
 	ID nonce.ID
+	// Hash is the record of the hash of the original message.
+	sha256.Hash
 	// First is the time the first piece was sent.
 	First time.Time
 	// Last is the time the last piece was sent.
 	Last time.Time
-	// Data is the amount of payload data.
-	Data int
-	// Redundancy is the amount of extra data that was sent.
-	Redundancy int
+	// Size is the number of bytes in the message payload.
+	Size int
+	// Ping is the recorded average current round trip time at send.
+	Ping time.Duration
 }
 
-// RxRecord is the details of a message reception and mostly forms the
-// data sent in a message received acknowledgement.
+// RxRecord is the details of a message reception and mostly forms the data sent
+// in a message received acknowledgement. The exported fields go into an
+// acknowledgement message.
 type RxRecord struct {
 	ID nonce.ID
+	// Hash is the hash of the reconstructed message received.
+	sha256.Hash
 	// First is when the first packet was received.
 	First time.Time
-	// Last is when the last packet arrived that led to successful
-	// reconstruction.
+	// Last is when the last packet was received. A longer time than the current
+	// ping RTT after First indicates retransmits.
 	Last time.Time
-	// Minimum is the size of the data packet.
-	Minimum int
-	// Required is the number of bytes that were collected for the eventual
-	// receiving.
-	Required int
+	// Size of the message as found in the packet headers.
+	Size int
+	// Received is the number of bytes received upon reconstruction, including
+	// packet overhead.
+	Received int
+	// Ping is the average ping RTT on the connection calculated at each packet
+	// receive, used with the total message transmit time to estimate an
+	// adjustment in the parity shards to be used in sending on this connection.
+	Ping time.Duration
 }
 
-// Segmenter is a message splitter/joiner and error correction adjustment
-// system that aims to minimise message latency by trading it for bandwidth
-// especially to cope with radio connections.
+// Segmenter is a message splitter/joiner and error correction adjustment system
+// that aims to minimise message latency by trading it for bandwidth especially
+// to cope with radio connections.
 type Segmenter struct {
 	*Listener
 	*crypto.KeySet
-	// MinReq is the count of packets for successful transmission.
-	MinReq int
-	// ActualReq is the amount of bytes that were needed to successfully
-	// transmit.
-	ActualReq int
+	// DataSent is the amount of actual bytes sent.
+	DataSent int
+	// ParitySent is the amount of bytes that were needed to successfully
+	// transmit, including packet overhead.
+	ParitySent int
 	// PingDivergence represents the proportion of time between start of send
 	// and receiving acknowledgement, versus the ping RTT being actively
-	// measured. Shorter time can reduce redundancy, longer time needs to
-	// increase it. Combined with MinReq / ActualReq this guides the error
-	// correction parameter for a given transmission that minimises latency.
-	// Value is a binary, fixed point value with 1<<1024 as "1".
+	// measured concurrently. Shorter time can reduce redundancy, longer time
+	// needs to increase it. Combined with DataSent / ParitySent this guides the
+	// error correction parameter for a given transmission that minimises
+	// latency. Value is a binary, fixed point value with 1<<32 as "1".
 	PingDivergence  int
 	PendingInbound  []*RxRecord
 	PendingOutbound []*TxRecord
