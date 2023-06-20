e943991f8fe0e522a4fe05753a0af1e25fd6a590
l0k18 <stalker.loki@protonmail.ch>
2023-06-20 12:14:21 +0100

    viper flags and tidier bits in engine

--- cmd/indra/relay/relay.go
+++ cmd/indra/relay/relay.go
@@ -4,6 +4,7 @@ import (
 	"github.com/indra-labs/indra"
 	log2 "github.com/indra-labs/indra/pkg/proc/log"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -13,18 +14,22 @@ var (
 
 var (
 	wireguardEnable bool
-	wireguardCIDR   string
+	wireguardCIDR   string // todo: there must be something like this. default route to 1
 	socksEnable     bool
 	socksListener   string
 )
 
 func Init(c *cobra.Command) {
 	relayCommand.PersistentFlags().BoolVarP(&wireguardEnable, "wireguard",
-		"w", false, "enable wiregfuard tunnel")
+		"w", false, "enable wireguard tunnel")
 	relayCommand.PersistentFlags().BoolVarP(&socksEnable, "socks",
 		"s", false, "enable socks proxy")
-	relayCommand.PersistentFlags().StringVar(&socksListener, "socks-listener",
-		"localhost:8080", "set address for socks 5 proxy listener")
+	relayCommand.PersistentFlags().StringVarP(&socksListener, "socks-listener",
+		"l", "localhost:8080", "set address for socks 5 proxy listener")
+
+	viper.BindPFlag("wireguard", relayCommand.PersistentFlags().Lookup("wireguard"))
+	viper.BindPFlag("socks", relayCommand.PersistentFlags().Lookup("socks"))
+	viper.BindPFlag("socks-listener", relayCommand.PersistentFlags().Lookup("socks-listener"))
 
 	c.AddCommand(relayCommand)
 }
@@ -33,6 +38,7 @@ var relayCommand = &cobra.Command{
 	Use:   "relay",
 	Short: "run a relay",
 	Long:  "Runs indra as a full relay, with optional client.",
+
 	Run: func(cmd *cobra.Command, args []string) {
 		log.I.Ln(log2.App.Load(), indra.SemVer)
 		nw, _ := cmd.Parent().PersistentFlags().GetString("network")
--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -48,142 +48,14 @@ type (
 		ShuttingDown atomic.Bool
 	}
 	Params struct {
-		Transport tpt.Transport
-		Listener  *transport.Listener
-		*crypto.Keys
+		Transport       tpt.Transport
+		Listener        *transport.Listener
+		Keys            *crypto.Keys
 		Node            *node.Node
 		Nodes           []*node.Node
 		NReturnSessions int
 	}
 )
-
-// Cleanup closes and flushes any resources the client opened that require sync
-// in order to reopen correctly.
-func (ng *Engine) Cleanup() {
-	// Do cleanup stuff before shutdown.
-}
-
-func (ng *Engine) GetHidden() *hidden.Hidden { return ng.h }
-
-func (ng *Engine) GetLoad() byte { return byte(ng.Load.Load()) }
-
-func (ng *Engine) HandleMessage(s *splice.Splice, pr ont.Onion) {
-	log.D.F("%s handling received message",
-		ng.Mgr().GetLocalNodeAddressString())
-	s.SetCursor(0)
-	s.Segments = s.Segments[:0]
-	on := reg.Recognise(s)
-	if on != nil {
-		log.D.Ln("magic", on.Magic())
-		if fails(on.Decode(s)) {
-			return
-		}
-		if pr != nil && on.Magic() != pr.Magic() {
-			log.D.S("", s.GetAll().ToBytes())
-		}
-		m := on.GetOnion()
-		if m == nil {
-			log.D.Ln("did not get onion")
-			return
-		}
-		if fails(m.(ont.Onion).Handle(s, pr, ng)) {
-			log.W.S("unrecognised packet", s.GetAll().ToBytes())
-		}
-	}
-}
-
-func (ng *Engine) Handler() (out bool) {
-	log.T.C(func() string {
-		return ng.Mgr().GetLocalNodeAddressString() + " awaiting message"
-	})
-	var prev ont.Onion
-	select {
-	case <-ng.ctx.Done():
-		ng.Shutdown()
-		out = true
-		break
-	case c := <-ng.Listener.Accept():
-		go func() {
-			log.D.Ln("new connection inbound (TODO):", c.Host.Addrs())
-			_ = c
-		}()
-	case b := <-ng.Mgr().ReceiveToLocalNode():
-		s := splice.Load(b, slice.NewCursor())
-		ng.HandleMessage(s, prev)
-	case p := <-ng.Mgr().GetLocalNode().PayChan.Receive():
-		log.D.F("incoming payment for %s: %v", p.ID, p.Amount)
-		topUp := false
-		ng.Mgr().IterateSessions(func(s *sessions.Data) bool {
-			if s.Preimage == p.Preimage {
-				s.IncSats(p.Amount, false, "top-up")
-				topUp = true
-				log.T.F("topping up %x with %v", s.Header.Bytes, p.Amount)
-				return true
-			}
-			return false
-		})
-		if !topUp {
-			ng.Mgr().AddPendingPayment(p)
-			log.T.F("awaiting session keys for preimage %s session Keys %s",
-				p.Preimage, p.ID)
-		}
-		// For now if we received this we return true. Later this will wait with
-		// a timeout on the lnd node returning the success to trigger this.
-		p.ConfirmChan <- true
-	case <-ng.Pause:
-		log.D.Ln("pausing", ng.Mgr().GetLocalNodeAddressString())
-		// For testing purposes we need to halt this Handler and discard channel
-		// messages.
-	out:
-		for {
-			select {
-			case <-ng.Mgr().GetLocalNode().PayChan.Receive():
-				log.D.Ln("discarding payments while in pause")
-			case <-ng.Mgr().ReceiveToLocalNode():
-				log.D.Ln("discarding messages while in pause")
-			case <-ng.ctx.Done():
-				break out
-			case <-ng.Pause:
-				// This will then resume to the top level select.
-				log.D.Ln("unpausing", ng.Mgr().GetLocalNodeAddressString())
-				break out
-			}
-		}
-	}
-	return
-}
-
-func (ng *Engine) Keyset() *crypto.KeySet           { return ng.KeySet }
-func (ng *Engine) WaitForShutdown() <-chan struct{} { return ng.ctx.Done() }
-func (ng *Engine) Mgr() *sess.Manager               { return ng.manager }
-func (ng *Engine) Pending() *responses.Pending      { return ng.Responses }
-func (ng *Engine) SetLoad(load byte)                { ng.Load.Store(uint32(load)) }
-
-// Shutdown triggers the shutdown of the client and the Cleanup before
-// finishing.
-func (ng *Engine) Shutdown() {
-	log.T.Ln("shutting down", ng.Mgr().GetLocalNodeAddress().String())
-	if ng.ShuttingDown.Load() {
-		return
-	}
-	ng.ShuttingDown.Store(true)
-	ng.Cleanup()
-	ng.cancel()
-}
-
-// Start a single thread of the Engine.
-func (ng *Engine) Start() {
-	log.T.Ln("starting engine")
-	if ng.sub != nil {
-		log.T.Ln("starting gossip handling")
-		ng.RunAdHandler(ng.HandleAd)
-	}
-	for {
-		if ng.Handler() {
-			break
-		}
-	}
-}
 
 // New creates a new Engine according to the Params given.
 func New(p Params) (ng *Engine, e error) {
@@ -231,4 +103,131 @@ func New(p Params) (ng *Engine, e error) {
 			nil, nil, 5))
 	}
 	return
+}
+
+// Cleanup closes and flushes any resources the client opened that require sync
+// in order to reopen correctly.
+func (ng *Engine) Cleanup() {
+	// Do cleanup stuff before shutdown.
+}
+
+func (ng *Engine) GetHidden() *hidden.Hidden { return ng.h }
+
+func (ng *Engine) GetLoad() byte { return byte(ng.Load.Load()) }
+
+func (ng *Engine) HandleMessage(s *splice.Splice, pr ont.Onion) {
+	log.D.F("%s handling received message",
+		ng.Mgr().GetLocalNodeAddressString())
+	s.SetCursor(0)
+	s.Segments = s.Segments[:0]
+	on := reg.Recognise(s)
+	if on != nil {
+		log.D.Ln("magic", on.Magic())
+		if fails(on.Decode(s)) {
+			return
+		}
+		if pr != nil && on.Magic() != pr.Magic() {
+			log.D.S("", s.GetAll().ToBytes())
+		}
+		m := on.GetOnion()
+		if m == nil {
+			log.D.Ln("did not get onion")
+			return
+		}
+		if fails(m.(ont.Onion).Handle(s, pr, ng)) {
+			log.W.S("unrecognised packet", s.GetAll().ToBytes())
+		}
+	}
+}
+
+func (ng *Engine) Handler() (terminate bool) {
+	log.T.C(func() string {
+		return ng.Mgr().GetLocalNodeAddressString() + " awaiting message"
+	})
+	var prev ont.Onion
+	select {
+	case <-ng.ctx.Done():
+		ng.Shutdown()
+		return true
+	case c := <-ng.Listener.Accept():
+		go func() {
+			log.D.Ln("new connection inbound (TODO):", c.Host.Addrs())
+			_ = c
+		}()
+	case b := <-ng.Mgr().ReceiveToLocalNode():
+		s := splice.Load(b, slice.NewCursor())
+		ng.HandleMessage(s, prev)
+	case p := <-ng.Mgr().GetLocalNode().PayChan.Receive():
+		log.D.F("incoming payment for %s: %v", p.ID, p.Amount)
+		topUp := false
+		ng.Mgr().IterateSessions(func(s *sessions.Data) bool {
+			if s.Preimage == p.Preimage {
+				s.IncSats(p.Amount, false, "top-up")
+				topUp = true
+				log.T.F("topping up %x with %v", s.Header.Bytes, p.Amount)
+				return true
+			}
+			return false
+		})
+		if !topUp {
+			ng.Mgr().AddPendingPayment(p)
+			log.T.F("awaiting session keys for preimage %s session Keys %s",
+				p.Preimage, p.ID)
+		}
+		// For now if we received this we return true. Later this will wait with a
+		// timeout on the ln node returning the success to trigger this.
+		p.ConfirmChan <- true
+	case <-ng.Pause:
+		log.D.Ln("pausing", ng.Mgr().GetLocalNodeAddressString())
+		// For testing purposes we need to halt this Handler and discard channel
+		// messages.
+	out:
+		for {
+			select {
+			case <-ng.Mgr().GetLocalNode().PayChan.Receive():
+				log.D.Ln("discarding payments while in pause")
+			case <-ng.Mgr().ReceiveToLocalNode():
+				log.D.Ln("discarding messages while in pause")
+			case <-ng.ctx.Done():
+				return true
+			case <-ng.Pause:
+				// This will then resume to the top level select.
+				log.D.Ln("unpausing", ng.Mgr().GetLocalNodeAddressString())
+				break out
+			}
+		}
+	}
+	return
+}
+
+func (ng *Engine) Keyset() *crypto.KeySet           { return ng.KeySet }
+func (ng *Engine) WaitForShutdown() <-chan struct{} { return ng.ctx.Done() }
+func (ng *Engine) Mgr() *sess.Manager               { return ng.manager }
+func (ng *Engine) Pending() *responses.Pending      { return ng.Responses }
+func (ng *Engine) SetLoad(load byte)                { ng.Load.Store(uint32(load)) }
+
+// Shutdown triggers the shutdown of the client and the Cleanup before
+// finishing.
+func (ng *Engine) Shutdown() {
+	log.T.Ln("shutting down", ng.Mgr().GetLocalNodeAddress().String())
+	if ng.ShuttingDown.Load() {
+		return
+	}
+	ng.ShuttingDown.Store(true)
+	ng.Cleanup()
+	ng.cancel()
+}
+
+// Start a single thread of the Engine.
+func (ng *Engine) Start() {
+	log.T.Ln("starting engine")
+	if ng.sub != nil {
+		log.T.Ln("starting gossip handling")
+		ng.RunAdHandler(ng.HandleAd)
+	}
+	for {
+		if ng.Handler() {
+			break
+		}
+	}
 }
