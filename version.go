6ab1bcb0fc9fa86ea1b1ded7b2af2f996c97176c
l0k18 <stalker.loki@protonmail.ch>
2023-07-06 09:28:11 +0100

    bump

--- version.go
+++ version.go
@@ -21,17 +21,17 @@ const (
 	// GitRef is the gitref, as in refs/heads/branchname.
 	GitRef = "refs/heads/master"
 	// ParentGitCommit is the commit hash of the parent HEAD.
-	ParentGitCommit = "da5d4c8ba58e8a398fafe7175a4b9e9362709ff1"
+	ParentGitCommit = "2e01ddf6d2e5b6e66ca0454902ae58cc38980792"
 	// BuildTime stores the time when the current binary was built.
-	BuildTime = "2023-07-01T21:28:07+01:00"
+	BuildTime = "2023-07-06T09:28:11+01:00"
 	// SemVer lists the (latest) git tag on the release.
-	SemVer = "v0.1.17"
+	SemVer = "v0.1.19"
 	// Major is the major number from the tag.
 	Major = 0
 	// Minor is the minor number from the tag.
 	Minor = 1
 	// Patch is the patch version number from the tag.
-	Patch = 17
+	Patch = 19
 )
 
 var CI = "false"
